58ec4d7b1e521365018fec774f16dcb952841906
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2019-11-01 12:30:56 -0400

    three events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+// Navigaton Links
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach(link => {
+  link.addEventListener("mouseover", () => {
+    link.style.transform = "scale(1,2)";
+    link.style.transition = "all 0.1s";
+  });
+  link.addEventListener("mouseleave", () => {
+    link.style.transform = "scale(1,1)";
+    link.style.transition = "all 0.1s";
+  });
+});
+
+// Logo
+const logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("click", () => {
+  logoHeading.textContent = "Ugly Bus";
+});
